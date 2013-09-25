579d675a3639989eeebebcc50281de81a6a92218
Pierre Schmitz <pierre@archlinux.de>
2013-09-25 19:03:19 +0200

    Add header for archlinux.de

---
+++ data/local/header.inc.php
@@ -0,0 +1,13 @@
+<div id="archnavbar" class="anb-paste">
+	<div id="archnavbarlogo"><h1><a href="/">Arch Linux</a></h1></div>
+	<div id="archnavbarmenu">
+	<ul id="archnavbarlist">
+		<li id="anb-start"><a href="https://www.archlinux.de/?page=Start">Start</a></li>
+		<li id="anb-packages"><a href="https://www.archlinux.de/?page=Packages">Pakete</a></li>
+		<li id="anb-forum"><a href="https://bbs.archlinux.de/">Forum</a></li>
+		<li id="anb-wiki"><a href="https://wiki.archlinux.de/">Wiki</a></li>
+		<li id="anb-download"><a href="https://wiki.archlinux.de/title/Download">Download</a></li>
+		<li id="anb-spenden"><a href="https://wiki.archlinux.de/title/Spenden">Spenden</a></li>
+	</ul>
+	</div>
+</div>
